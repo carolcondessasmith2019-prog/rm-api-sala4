41ba8a713c4a143e7f9af0b845f32b3c8b511368
carolcondessasmith2019-prog <carolcondessasmith2019@gmail.com>
2026-06-02 09:40:50 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import logo from '/logo.png'
 
 function App() {
   const [data, setData] = useState([])
+  const [page, setPage]
 
   useEffect(() => {
-    api.get(`/character`).then((response) => {
+    api.get(`/character/page=${page}&name=${name}`).then((response) => {
       setData(response.data.results)
     }).catch((error) => {
       console.error("Deu ruim!!!", error)
@@ -18,9 +19,9 @@ function App() {
   return (
     <>
       <img className={s.logo} src={logo} alt="Logo Rick and Morty" />
-      <div>
+      <div className={s.search}>
         <label>Search name</label>
-        <input type="text" placeholder='Type the name you want'/>
+        <input type="text" placeholder='Type the name you want'value={name} onChange={(e)=>setName}/>
       </div>
       <main>
         {data.map((item, index) => {
@@ -28,6 +29,9 @@ function App() {
             <div>
               <h2>{item.name}</h2>
               <img src={item.image} alt="" />
+              <h4>Name: {item.image}</h4>
+              <p>Species: {item.species}</p>
+                {item.status === "Dead" ? "Status:" }
             </div>
           )
         })}
